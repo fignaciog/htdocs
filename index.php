4ed5d5dd1592146939a54ef7f81d49f1c5e31f6f
fignaciog <37256653+fignaciog@users.noreply.github.com>
2018-03-12 22:11:39 -0400

    Prototipo.0.5
    - Vista del index.

--- index.php
+++ index.php
@@ -9,71 +9,83 @@
 </head>
 <body>
 
-
-    <div class="row">
-
-        <div class="col-sm-4 bg-light">
-            <img src="img\logo_ejemplo.png" class="img-fluid rounded" alt="">
-        </div>
-
-        <div class="col-sm-8">
-            <h1>Intranet</h1>
-            <footer class="blockquote-footer">Ignacio Fortuna</footer>
-        </div>
-
-    </div>
-
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1>Projectos</h1>
+            <h1>Ignacio's Projects</h1>
         </div>
     </div>
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col bg-info text-white">
-            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
-            <p>
-                Detalles del proyecto
-            </p>
-            <a href="Mobile" class="btn btn-light">
-            Diseño Phone
-            </a>
-        </div>
-
-        <div class="col">
-            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
-            <p>
-                Detalles del proyecto
-            </p>
-            <a href="Desktop" class="btn btn-light">Diseño Desktop</a>
-        </div>
-
-        <div class="col bg-info text-white">
-            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
-            <p>
-                Detalles del proyecto
-            </p>
-            <a href="Escuela" class="btn btn-light">Projecto Escuela</a>
-        </div>
-
-        <div class="col">
-            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
-            <p>
-                Detalles del proyecto
-            </p>
-            <a href="php" class="btn btn-light">Repaso Php</a>
-        </div>
-
-        <div class="col bg-info text-white">
-            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
-            <p>
-                Detalles del proyecto
-            </p>
-            <a href="www.BackEnd.com" class="btn btn-light">Repaso Back-End</a>
-        </div>
         
+        <div class="card col-sm col-md col-lg col-xl">
+            <img src="img\img_ejemplo.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Diseño Movil</h5>
+                <p class="card-text">
+                    Detalles del proyecto
+                </p>
+                <a href="Mobile" class="btn btn-light">
+                    Entrar
+                </a>
+            </div>
+        </div>
+
+        <div class="card col-sm col-md col-lg col-xl">
+            <img src="img\img_ejemplo.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Diseño Escritorio</h5>
+                <p class="card-text">
+                    Detalles del proyecto
+                </p>
+                <a href="Desktop" class="btn btn-light">
+                Entrar
+                </a>
+            </div>
+        </div>
+
+        <div class="card col-sm col-md col-lg col-xl">
+            <img src="img\img_ejemplo.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Sistema Escuela</h5>
+                <p class="card-text">
+                    Sistema de registro estudiantil.
+                </p>
+                <a href="Escuela" class="btn btn-light">
+                Entrar
+                </a>
+            </div>
+        </div>
+
+        <div class="card col-sm col-md col-lg col-xl">
+            <img src="img\img_ejemplo.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Learning Php</h5>
+                <p class="card-text">
+                    Repaso de Php
+                </p>
+                <a href="php" class="btn btn-light">
+                Entrar
+                </a>
+            </div>
+        </div>
+
+        <div class="card col-sm col-md col-lg col-xl">
+            <img src="img\img_ejemplo.png" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Learning MVC</h5>
+                <p class="card-text">
+                    Aprendiendo el Modelo, Vista, Controlador
+                </p>
+                <a href="www.BackEnd.com" class="btn btn-light">
+                Entrar
+                </a>
+            </div>
+        </div>
     </div>
+    <footer class="blockquote-footer">
+Intranet_Ignacio Fortuna
+</footer>
 </div>
 </body>
 </html>
