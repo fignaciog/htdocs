fea1c6aeccd015efc6e69772de74ac1e5d5069b7
fignaciog <37256653+fignaciog@users.noreply.github.com>
2018-03-12 20:54:14 -0400

    Bloques
    - Bloques <div>

--- index.php
+++ index.php
@@ -8,49 +8,72 @@
     <title>Intranet | Inicio</title>
 </head>
 <body>
+
+
+    <div class="row">
+
+        <div class="col-sm-4 bg-light">
+            <img src="img\logo_ejemplo.png" class="img-fluid rounded" alt="">
+        </div>
+
+        <div class="col-sm-8">
+            <h1>Intranet</h1>
+            <footer class="blockquote-footer">Ignacio Fortuna</footer>
+        </div>
+
+    </div>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1>Projectos</h1>
+        </div>
+    </div>
+
 <div class="container-fluid">
-<div class="row">
-    <div class="col-sm-4" style="background-color:red;">
-    <p>
-    Logo
-    </p>
+    <div class="row">
+        <div class="col bg-info text-white">
+            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
+            <p>
+                Detalles del proyecto
+            </p>
+            <a href="Mobile" class="btn btn-light">
+            Diseño Phone
+            </a>
+        </div>
+
+        <div class="col">
+            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
+            <p>
+                Detalles del proyecto
+            </p>
+            <a href="Desktop" class="btn btn-light">Diseño Desktop</a>
+        </div>
+
+        <div class="col bg-info text-white">
+            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
+            <p>
+                Detalles del proyecto
+            </p>
+            <a href="Escuela" class="btn btn-light">Projecto Escuela</a>
+        </div>
+
+        <div class="col">
+            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
+            <p>
+                Detalles del proyecto
+            </p>
+            <a href="php" class="btn btn-light">Repaso Php</a>
+        </div>
+
+        <div class="col bg-info text-white">
+            <img src="img\img_ejemplo.png" class="img-fluid img-thumbnail" alt="">
+            <p>
+                Detalles del proyecto
+            </p>
+            <a href="www.BackEnd.com" class="btn btn-light">Repaso Back-End</a>
+        </div>
+        
     </div>
-    <div class="col-sm-8">
-    <h1>Intranet Ignacio Fortuna</h1>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
-        <p>
-            Detalles del proyecto
-        </p>
-        <a href="Mobile">Diseño Phone</a>
-    </div>
-    <div class="col">
-        <p>
-            Detalles del proyecto
-        </p>
-        <a href="Desktop">Diseño Desktop</a>
-    </div>
-    <div class="col">
-        <p>
-            Detalles del proyecto
-        </p>
-        <a href="Escuela">Projecto Escuela</a>
-    </div>
-    <div class="col">
-        <p>
-            Detalles del proyecto
-        </p>
-        <a href="php">Repaso Php</a>
-    </div>
-    <div class="col">
-        <p>
-            Detalles del proyecto
-        </p>
-        <a href="www.BackEnd.com">Repaso Back-End</a>
-    </div>
-</div>
 </div>
 </body>
 </html>
