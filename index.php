064e29f07492e1a75ec4fe913dae2b20af0e3d1f
fignaciog <37256653+fignaciog@users.noreply.github.com>
2018-03-14 17:41:53 -0400

    Estable

--- index.php
+++ index.php
@@ -6,78 +6,89 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css\bootstrap.min.css">    
     <title>Intranet | Inicio</title>
+
+    <style>
+    
+    .imgEje {
+        background-size: 50%;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+    
+    </style>
 </head>
 <body>
 
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1>Ignacio's Projects</h1>
+            <h1 class="display-4">Ignacio's Projects</h1>
         </div>
     </div>
 
 <div class="container-fluid">
     <div class="row">
-             <div class="col border border-primary ">
+
+             <div class="col-sm col-md col-lg col-xl-3 bg-light text-black d-block rounded m-3">
                 <img src="img\img_phone.png" class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Diseño Movil</h5>
-                    <p class="card-text">
+                    <p class="card-text text-sm-left">
                         Detalles del proyecto
                     </p>
-                    <a href="Mobile" class="btn btn-light">
-                        Entrar
+                    <a href="Mobile" class="btn btn-secondary">
+                        Ver..
                     </a>
                 </div>
             </div>
 
-            <div class="col border border-primary">
+            <div class="col-sm col-md col-lg col-xl-3 bg-light text-black d-block rounded m-3">
                 <img src="img\img_desktop.png" class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Diseño Escritorio</h5>
                     <p class="card-text">
                         Detalles del proyecto
                     </p>
-                    <a href="Desktop" class="btn btn-light">
-                    Entrar
+                    <a href="Desktop" class="btn btn-secondary">
+                    Ver..
                     </a>
                 </div>
             </div>
 
-            <div class="col border border-primary">
-                <img src="img\img_Sistema_R.png" class="card-img-top" alt="">
+            <div class="col-sm col-md col-lg col-xl-3 bg-light text-black d-block rounded m-3">
+                <img src="img\img_registro.png" class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Sistema Registro</h5>
                     <p class="card-text">
                         Sistema de registro estudiantil.
                     </p>
-                    <a href="Escuela" class="btn btn-light">
-                    Entrar
+                    <a href="Escuela" class="btn btn-secondary">
+                    Ver..
                     </a>
                 </div>
             </div>
 
-            <div class="col border border-primary">
+            <div class="col-sm col-md col-lg col-xl-3 bg-light text-black d-block rounded m-3">
                 <img src="img\img_php.png" class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Learning Php</h5>
                     <p class="card-text">
                         Repaso de Php
                     </p>
-                    <a href="php" class="btn btn-light">
-                    Entrar
+                    <a href="php" class="btn btn-secondary">
+                    Ver..
                     </a>
                 </div>
             </div>
 
-            <div class="col border border-primary">
+            <div class="col-sm col-md col-lg col-xl-3 bg-light text-black d-block rounded m-3">
                 <img src="img\img_mvc.png" class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Learning MVC</h5>
                     <p class="card-text">
                         Aprendiendo el Modelo, Vista, Controlador
                     </p>
-                    <a href="www.BackEnd.com" class="btn btn-light">
-                    Entrar
+                    <a href="www.BackEnd.com" class="btn btn-secondary">
+                    Ver..
                     </a>
                 </div>
             </div>       
